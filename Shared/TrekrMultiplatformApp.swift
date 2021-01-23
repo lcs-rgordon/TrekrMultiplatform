9858d59a7f1a3cf8604403d13d519e4bab023aad
Russell Gordon <rgordon@lcs.on.ca>
2021-01-23 08:42:41 -0500

    Initial Commit

---
+++ Shared/TrekrMultiplatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrMultiplatformApp.swift
+//  Shared
+//
+//  Created by Russell Gordon on 2021-01-23.
+//
+
+import SwiftUI
+
+@main
+struct TrekrMultiplatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
