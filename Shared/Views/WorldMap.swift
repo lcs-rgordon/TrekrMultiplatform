5ffb31c120bd99cf9a54bca280b151dcaa326c16
Russell Gordon <rgordon@lcs.on.ca>
2021-01-24 15:58:36 -0500

    Added tips and map view, and tab bar to switch between them.

---
+++ Shared/Views/WorldMap.swift
@@ -0,0 +1,20 @@
+//
+//  WorldMap.swift
+//  TrekrMultiplatform
+//
+//  Created by Russell Gordon on 2021-01-24.
+//
+
+import SwiftUI
+
+struct WorldMap: View {
+    var body: some View {
+        Text("Hello, WorldMap view!")
+    }
+}
+
+struct WorldMap_Previews: PreviewProvider {
+    static var previews: some View {
+        WorldMap()
+    }
+}
