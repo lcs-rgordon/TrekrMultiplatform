63b8a5a6b96d18d394d239cf0f38b17115a7cbbb
Russell Gordon <rgordon@lcs.on.ca>
2021-01-24 15:20:42 -0500

    Revise location example property to UK.

--- Shared/Model/Location.swift
+++ Shared/Model/Location.swift
@@ -19,15 +19,15 @@ struct Location: Decodable {
     let advisory: String
    
     // "static" – exists at Type level – shared across the type, created once
-    static let example = Location(id: 2,
-                           name: "Great Smoky Mountains",
-                           country: "United States",
-                           description: "The Great Smoky Mountains are a mountain range rising along the Tennessee–North Carolina border in the southeastern United States. They are a subrange of the Appalachian Mountains, and form part of the Blue Ridge Physiographic Province. The range is sometimes called the Smoky Mountains and the name is commonly shortened to the Smokies. The Great Smokies are best known as the home of the Great Smoky Mountains National Park, which protects most of the range. The park was established in 1934, and, with over 11 million visits per year, it is the most visited national park in the United States.",
-                           more: "The Great Smokies are part of an International Biosphere Reserve. The range is home to an estimated 187,000 acres (76,000 ha) of old growth forest, constituting the largest such stand east of the Mississippi River. The cove hardwood forests in the range's lower elevations are among the most diverse ecosystems in North America, and the Southern Appalachian spruce-fir forest that coats the range's upper elevations is the largest of its kind. The Great Smokies are also home to the densest black bear population in the Eastern United States and the most diverse salamander population outside of the tropics.",
-                           latitude: 35.6532,
-                           longitude: -83.5070,
-                           heroPicture: "smokies",
-                           advisory: "We accept no liability for any visitors eaten alive by bears.")
+    static let example = Location(id: 1,
+                           name: "The Highlands",
+                           country: "United Kingdom",
+                           description: "The Highlands is a historic region of Scotland. Culturally, the Highlands and the Lowlands diverged from the later Middle Ages into the modern period, when Lowland Scots replaced Scottish Gaelic throughout most of the Lowlands. The term is also used for the area north and west of the Highland Boundary Fault, although the exact boundaries are not clearly defined, particularly to the east. The Great Glen divides the Grampian Mountains to the southeast from the Northwest Highlands. The Scottish Gaelic name of A' Ghàidhealtachd literally means \"the place of the Gaels\" and traditionally, from a Gaelic-speaking point of view, includes both the Western Isles and the Highlands.",
+                           more: "The area is very sparsely populated, with many mountain ranges dominating the region, and includes the highest mountain in the British Isles, Ben Nevis. During the 18th and early 19th centuries the population of the Highlands rose to around 300,000, but from c. 1841 and for the next 160 years, the natural increase in population was exceeded by emigration (mostly to Canada, the United States, Australia and New Zealand, and migration to the industrial cities of Scotland and England.) The area is now one of the most sparsely populated in Europe. At 9.1/km2 (24/sq mi) in 2012, the population density in the Highlands and Islands is less than one seventh of Scotland's as a whole, comparable with that of Bolivia, Chad and Russia.",
+                           latitude: 57.1200,
+                           longitude: -4.7100,
+                           heroPicture: "highlands",
+                           advisory: "We accept no liability for any visitors eaten alive by midges.")
 }
 
 
